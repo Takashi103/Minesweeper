1306fd98a601a4f6849bbe6f32e5b97ff6624174
MauveRanger <mpeschel@outlook.com>
2017-01-13 18:25:26 -0500

    Create the game source file
    Just a placeholder for now.

---
+++ Minesweeper.js
@@ -0,0 +1,7 @@
+/* MauveRanger
+Supposed to be the main code for the game. I have no idea how to do this.
+*/
+
+alert("THE GAME IS NOT WORKING");
+alert("but the code is loading");
+alert("so that's something at least");
