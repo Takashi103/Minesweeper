1d838b53c0191eabe39da3669f53ae8c650534d3
Takashi103 <01bobiu@gmail.com>
2017-01-13 22:03:54 -0500

    Update Minesweeper.js

--- Minesweeper.js
+++ Minesweeper.js
@@ -2,6 +2,10 @@
 Supposed to be the main code for the game. I have no idea how to do this.
 */
 
-alert("THE GAME IS NOT WORKING");
-alert("but the code is loading");
-alert("so that's something at least");
+function setup() {
+	
+}
+
+function draw() {
+	
+}
