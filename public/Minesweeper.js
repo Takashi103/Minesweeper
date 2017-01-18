f6f5fd238556f749e863a2804ca3ba7a9070dc45
Takashi103 <01bobiu@gmail.com>
2017-01-17 20:37:12 -0500

    Create Minesweeper.js

---
+++ public/Minesweeper.js
@@ -0,0 +1,11 @@
+/* MauveRanger
+Supposed to be the main code for the game. I have no idea how to do this.
+*/
+
+function setup() {
+	socket = io.connect('http://localhost:3000')
+}
+
+function draw() {
+
+}
