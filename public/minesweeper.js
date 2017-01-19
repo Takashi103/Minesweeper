e053c9aaa9c5724cd45028055d9333f21a0b35c9
MauveRanger <mpeschel@outlook.com>
2017-01-18 20:52:49 -0500

    Updates
    updates

--- public/minesweeper.js
+++ public/minesweeper.js
@@ -1,38 +1,65 @@
-window.onload = function() {
+var socket = io();
+var tileBoard;
+var tileWidth = 16;
+var tileHeight = 16;
+var rows;
+var columns;
 
-    socket = io.connect('http://localhost:3000');
-    var blankTile;
-    blankTile = new Image();
-        
-    //I had some problems with the code rolling forwards without the image loading first.
-    //The image size would be returned as zero, and the code would try to draw infinite tiles.
-    //And that's bad.
-        
-    //This sets it up so's the drawGrid method gets called once the image FINISHES loading.
-    blankTile.onload = function (){
-        //Obviously, the first argument is the canvas, and the second is the image itself.
-        drawGrid(document.getElementById("myCanvas"), this);
-    };
-    //Load the image. onLoad() is triggered once the image is present, and calls drawGrid().
-    blankTile.src = "/images/blank.gif";
-};
+$(document).ready(function(){
+	//Initialize variables.
+    console.log('jQuery has loaded.');
+
+	tileBoard = document.getElementById("myCanvas");
+
+	//When we receive rows and columns from server, draw the grid.
+    socket.on('settings', function (rows, cols) {
+        console.log("Rows: " + rows + ' Columns: ' + cols);
+		this.rows = rows;
+		this.columns = cols;
+		tileBoard.width = tileWidth * cols;
+		tileBoard.height = tileHeight * rows;
+		tileBoard.style = "border:1px solid #000000;";
+
+
+		var blankTile;
+		blankTile = new Image();
+
+		blankTile.onload = function (){
+			drawGrid(tileBoard, this);
+		};
+		blankTile.src = "/images/blank.gif";
+
+	});
+	
+	tileBoard.onclick = takeClick;
+
+});
+	
+function takeClick(mouseEvent)
+{
+	var x = mouseEvent.clientX;
+	var y = mouseEvent.clientY;
+	
+	x -= this.offsetLeft;
+	y -= this.offsetTop;
+	
+	x = Math.floor(x / tileWidth);
+	y = Math.floor(y / tileHeight);
+	
+	
+	//Check if the tile has not been revealed
+	console.log("x: " + x + " y: " + y);
+	if(true)  //Except, at this point, there is no reveal. :/
+		socket.emit('tileClick', x, y);		//Send x and y of tiles to server
+}
+
 function drawGrid(c, tile) 
 {
-    //console.log("drawGrid has been called");
-    var tileWidth = tile.naturalWidth;
-    var tileHeight = tile.naturalHeight;
-    
-    //These can tell you if the image is loaded or not.
-    //If you're having problems with missing tiles, uncomment them and check that they're not zero or whatever.
-    //console.log("tileWidth is " + tileWidth);
-    //console.log("tileHeight is " + tileHeight);
-    
-    context = c.getContext("2d");
+	context = c.getContext("2d");
 
-    for(var y = 0; y < c.height; y += tileHeight) {     //Draw rows until all rows full
-        for(var x = 0; x < c.width; x += tileWidth) {   //Draw tiles until row is full
-            context.drawImage(tile, x, y);
-        }
-    }
-    //console.log("drawGrid has closed successfully");
+	for(var y = 0; y < c.height; y += tileHeight) {		//Draw rows until all rows full
+		for(var x = 0; x < c.width; x += tileWidth) {	//Draw tiles until row is full
+			context.drawImage(tile, x, y);
+		}
+	}
 }
