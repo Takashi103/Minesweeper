5272d913ce6d727ddf9a13fa34366ee22a3174a1
Takashi103 <01bobiu@gmail.com>
2017-01-25 17:37:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -15,14 +15,34 @@ var io = require('socket.io')(server);
 
 //--------------------------------------------
 
+function Tile (x, y) {
+	this.x = x;
+	this.y = y;
+	this.mine = false;
+}
+
 var rows = 10;
 var cols = 10;
+var mines = 30;
 
-/*
-var grid = new Array(cols);
-for (var i = 0; i < cols; i++) {
-  grid[i] = new Array(rows);
-}*/
+var board = new Array(cols);
+for(var i = 0; i < board.length; i++)
+	board(i) = new Array(rows);
+
+for(var i = 0; i < board.length; i++)
+	for(var j = 0; j < board(i).length; j++)
+		board(i)(j) = new Tile(i)(j);
+
+for(var i = 0; i <= mines; i++) {
+	var a = ((Math.random() * rows) + 1);
+	var b = ((Math.random() * cols) + 1);
+	if(board(a)(b).mine)
+		i--;
+	else {
+		board(a)(b).mine = true;
+		console.log(board(a)(b).x + " " + board(a)(b).y)
+	}
+}
 
 io.on('connection', function(socket){
 	console.log('new user connected with id: ' + socket.id);
