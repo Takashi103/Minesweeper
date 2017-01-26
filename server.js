689e9a89d0e3f93350dfe27dc70d09a95a5da1e6
MauveRanger <mpeschel@outlook.com>
2017-01-25 19:43:35 -0500

    Send updateboard if mine
    New method: If a mine is encountered, sends an update to the client telling them to draw it in. Update contains a Tile object, which has been redefined to contain a "content" value (0-8 for empty tiles, 9 for mines).

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ var io = require('socket.io')(server);
 
 //--------------------------------------------
 
-function Tile (x, y) {
+function Tile(x, y, content) {
 	this.x = x;
 	this.y = y;
-	this.mine = false;
+	this.content = content;
 }
 
 var rows = 10;
@@ -27,29 +27,36 @@ var mines = 30;
 
 var board = new Array(cols);
 for(var i = 0; i < board.length; i++)
-	board(i) = new Array(rows);
+	board[i] = new Array(rows);
 
 for(var i = 0; i < board.length; i++)
-	for(var j = 0; j < board(i).length; j++)
-		board(i)(j) = new Tile(i)(j);
+	for(var j = 0; j < board[i].length; j++)
+		board[i][j] = new Tile(i, j, 0);
 
 for(var i = 0; i <= mines; i++) {
-	var a = ((Math.random() * rows) + 1);
-	var b = ((Math.random() * cols) + 1);
-	if(board(a)(b).mine)
+	var a = Math.floor(Math.random() * rows);
+	var b = Math.floor(Math.random() * cols);
+	if(board[a][b].content == 9)
 		i--;
 	else {
-		board(a)(b).mine = true;
-		console.log(board(a)(b).x + " " + board(a)(b).y)
+		board[a][b].content = 9;
+		console.log(board[a][b].x + " " + board[a][b].y)
 	}
 }
+
 
 io.on('connection', function(socket){
 	console.log('new user connected with id: ' + socket.id);
 	socket.emit('settings', rows, cols);
-
 	socket.on('tileClick', function(x, y) {
 		console.log('Click recieved at: ' + x +', ' + y);
+		if(board[x][y].content == 9)
+		{
+			var myCreation = {};
+			myCreation.data = new Array(1);
+			myCreation.data[0] = new Tile(x, y, 9);
+			console.log("myCreation: " + myCreation.data[0].x + ", " + myCreation.data[0].y + ", " + myCreation.data[0].content);
+			this.emit('boardupdate', myCreation);
+		}
 	});
-
 });
