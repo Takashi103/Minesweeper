247cbd8397bf757cd695c1cd353de8d0d9afa497
MauveRanger <mpeschel@outlook.com>
2017-02-03 19:55:22 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 /*
 Takashi: js file for server
 */
@@ -43,23 +44,54 @@ for(var i = 0; i <= mines; i++) {
 	}
 }
 
-tileFlipper = new revealStack(cols, rows, board, function(tile){
-	console.log("Show tile at " + tile.x + ", " + tile.y);
-});
+tileFlipper = new revealStack(cols, rows, board)
+
+for (var i = 0; i < cols; i++) {
+	for (var j = 0; j < rows; j++) {
+		if(board[i][j].content != 9) {
+
+			var sum = 0;
+			var adjacentContent = nearbyMines(i, j);
+
+			for (var k = 0; k < adjacentContent.length; k++) {
+				if(adjacentContent[k] == 9)
+					sum++;
+			}
+			board[i][j].content = sum;
+		}
+	}
+}
+
+function nearbyMines(x, y) {
+	var mineCount = 0;
+	var tiles = new Array;
+	//Row above
+	inBounds(x - 1, y - 1);
+	inBounds(x, y - 1);
+	inBounds(x + 1, y - 1);
+	//Left and right
+	inBounds(x - 1, y);
+	inBounds(x + 1, y);
+	//Row below
+	inBounds(x - 1, y + 1);
+	inBounds(x, y + 1);
+	inBounds(x + 1, y + 1);
+	function inBounds(x, y) {
+	//Check that the tile is on the board
+		if(x >= 0 && y >= 0 && x < cols && y < rows)
+			tiles.push(board[x][y].content);
+	}
+	return tiles;
+}
+
 
 io.on('connection', function(socket){
 	console.log('new user connected with id: ' + socket.id);
 	socket.emit('settings', rows, cols);
 	socket.on('tileClick', function(x, y) {
-		console.log('Click recieved at: ' + x +', ' + y);
-		
-		
-		
-		//This is the problem. For some reason, bigReveal is not getting the tile, I think?
-		tileFlipper.bigReveal(board[x][y]);
-		
-		
-		
+		console.log('Click recieved at: ' + x +', ' + y + ', Content = ' + board[x][y].content);
+		var revealedTiles = tileFlipper.bigReveal(board[x][y]);
+		socket.emit('boardupdate', {data: revealedTiles});
 	});
 });
 
@@ -74,49 +106,50 @@ function Tile(x, y, content) {
 
 
 
-
-
-
-
 //Supposed to return an object for revealing a contiguous block of blank tiles
-//                  (int,   int,    Tile[][], function )
-function revealStack(cols, rows, board, revealMethod) {
+function revealStack(cols, rows, board) {
+
 	this.cols = cols;
 	this.rows = rows;
-	this.board = board;
+	this.board = board;					//The complete set of tiles. It's a two-level array.
 
-	var revealMethod = revealMethod;	//External method that we call to reveal a given tile.
-	var tileStack = [];	//List of tiles to reveal
+	var tileQueue = [];					//List of tiles to reveal
 
 	//This is apparently how you make private methods in JavaScript:
 	//var methodName = function(parameters) {doStuff;};
 	
 	//Reveals the center tile, then the things around it. Reveals even if the first tile is
-	//a mine. Also, because tile.reveal gets set independent of calls to the revealMethod,
+	//a mine. Also, because the tile.reveal value gets set independent of calls to the revealMethod,
 	//interrupting this method may leave tiles marked as revealed without actually showing them
-	//according to revealMethod().
+	//using revealMethod().
 	this.bigReveal = function(workingTile) {
+		
+		console.log("bigReveal!");
 		//Start working with the first tile. If it's not valid for the clicky-clicky, pushTile()
-		//won't add it to the tileStack and we'll just skip the loop.
-		pushTile(workingTile);
+		//won't add it to the tileQue and we'll just skip the loop.
+		var revealedMines = [];
+		pushTile(workingTile.x, workingTile.y);
 		//Loop: take a tile off the bottom of the array. Maybe put some tiles on top.
 		//Lather, rinse, repeat.
-		while(tileStack.length > 0)
+		while(tileQueue.length > 0)
 		{
-			//Pull the next item from the stack
-			workingTile = tileStack.shift;
+			//Pull the next item from the que.
+			//In JavaScript, shift removes the first item in the array.
+			workingTile = tileQueue.shift();
 			//If it's a blank tile, mark nearby tiles to reveal.
 			//Note: pushTile automatically excludes tiles off the board and tiles already shown.
 			if(workingTile.content == 0)
 				pushAbout(workingTile.x, workingTile.y);
 			//Reveal the tile. Assume revealMethod has been set to make the mine show up onscreen,
 			//make the server ping the users, etc. etc.
-			revealMethod(workingTile);
+			revealedMines.push(workingTile);
 		}
+		
+		return revealedMines;
 	};
-	//I could use nested for-loops for this
-	//but that's hard and I'm lazy
-	//Calls pushTile, which checks for us if the tile is marked or out of range.
+
+	//I could use nested for-loops for this but that's hard and I'm lazy.
+	//Calls pushTile, which checks for us if the tile is already marked or out of range.
 	var pushAbout = function(x, y) {
 		//Row above
 		pushTile(x - 1, y - 1);
@@ -128,23 +161,21 @@ function revealStack(cols, rows, board, revealMethod) {
 		//Row below
 		pushTile(x - 1, y + 1);
 		pushTile(x, y + 1);
-		pushTile(x + 1, y + 1);
+		pushTile(x + 1, y + 1);		
 	};
+
 	var pushTile = function(x, y){
-		console.log("Pushtile has been called");
-		//Check that the tile is on the board
-		console.log("The tile bounds are " + x + ", " + y);
+		console.log("Push tile " + x + ", " + y);
+		//Check that the tile is on the board.
 		if(x >= 0 && y >= 0 && x < cols && y < rows)
 		{
-			console.log("The tile is in bounds, " + x + ", " + y);
+			//Get the tile object from the board.
 			var tile = board[x][y];
-			//Check that the tile is not already on the stack
+			//Check that the tile is not already in the que.
 			if(!tile.revealed)
 			{
-				console.log("The tile is not revealed, " + tile.revealed);
-				//Mark that the tile is being added to the stack
-				tile.revealed = true;
-				tileStack.push(tile);	//Add it to the end of the array
+				tile.revealed = true;	//Mark that the tile is being added to the que.
+				tileQueue.push(tile);	//Add it to the end of the array.
 			}
 		}
 	};
